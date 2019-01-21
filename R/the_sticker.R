94a3ec0f597d93c012dedfec7335825e4ccf11a1
fhernanb <fhernanb@unal.edu.co>
2019-01-21 11:17:34 -0500

    pruebas

---
+++ R/the_sticker.R
@@ -0,0 +1,13 @@
+library(hexSticker)
+
+sticker(subplot='my_documents/plantilla_logo.png',
+        package="stests",
+        h_fill='#C06C84',
+        p_color='#355C7D',
+        h_color='#F8B195',
+        p_size=45,
+        s_x=1,
+        s_y=.8,
+        s_width=0.9,
+        s_height=0.6,
+        filename="my_documents/logo.png")
