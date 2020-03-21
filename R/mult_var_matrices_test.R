7c6dd3cfd64e4b07b45a14ffc36b1a64c01a6002
fhernanb <fhernanb@unal.edu.co>
2020-03-21 17:02:19 -0500

    we added the Box test for homogeneity covariance matrices

---
+++ R/mult_var_matrices_test.R
@@ -0,0 +1,92 @@
+#' Tests for homogeneity of covariances matrices
+#'
+#' The function implements the test for \eqn{H_0: \Sigma_1 = \Sigma_2 = ... = \Sigma_q} versus \eqn{H_1} at least one \eqn{\Sigma_i} is different.
+#'
+#' @param s a list with the sample covariance matrices.
+#' @param n a list with the sample sizes.
+#' @param method a character string specifying the method, "box" (default), please see the details section for other methods.
+
+#' @details the \code{"method"} must be one of \code{"box"} (default), \code{"xxx"} (pronto otros metodos).
+#'
+#' @return A list with class \code{"htest"} containing the following components:
+#' \item{statistic}{the value of the statistic.}
+#' \item{parameter}{the degrees of freedom for the test.}
+#' \item{p.value}{the p-value for the test.}
+#' \item{estimate}{the estimated mean vectors.}
+#' \item{method}{a character string indicating the type of test performed.}
+#'
+#' @author Freddy Hernandez.
+#' @examples
+#' # Example 5.2.3 from Diaz and Morales (2015) page 200
+#' s1 <- matrix(c(12.65, -16.45,
+#'                -16.45, 73.04), ncol=2, nrow=2)
+#' s2 <- matrix(c(11.44, -27.77,
+#'                -27.77, 100.64), ncol=2, nrow=2)
+#' s3 <- matrix(c(14.46, -31.26,
+#'                -31.26, 101.03), ncol=2, nrow=2)
+#' n1 <- 26
+#' n2 <- 23
+#' n3 <- 25
+#' s <- list(s1, s2, s3)
+#' n <- list(n1, n2, n3)
+#' res <- mult_var_matrices_test(s, n, method="box")
+#' res
+#' plot(res, shade.col='tomato')
+#'
+#' @importFrom stats pf
+#' @export
+mult_var_matrices_test <- function(s, n, method='box') {
+
+  if (! var(unlist(lapply(s, dim))) == 0)
+    stop("All matrices in list s must have identical dimension")
+
+  if (! identical(length(s), length(n)))
+    stop("The length of the lists s and n do not match")
+
+  method <- match.arg(arg=method,
+                      choices=c("box"))
+
+  # To generate the code for evaluating, without using cases
+  my_code <- paste0("mult_var_matrices_test_", method,
+                    "(s, n)")
+
+  # To obtain the result
+  result <- eval(parse(text=my_code))
+  class(result) <- "htest"
+  return(result)
+}
+#' @importFrom stats pchisq
+mult_var_matrices_test_box <- function(s, n) {
+  p <- nrow(s[[1]])   # number of variables
+  q <- length(s)      # number of groups
+  N <- sum(unlist(n))
+  v <- N - q
+  vg <- unlist(n) - 1
+  aux1 <- sum(1/vg) - 1/v
+  rho <- 1 - (2*p^2+3*p-1) * aux1 / (6*(p+1)*(q-1))
+  # To obtian log(lambda)
+  aux2 <- Map("*", s, vg)
+  Sp <- Reduce("+", aux2) / v
+  aux3 <- sum(log(unlist(lapply(s, det))) * vg)
+  log_lambda <- (v * log(det(Sp)) - aux3) / (-2)
+  # The statistic
+  fi <- -2 * rho * log_lambda
+  p.value <- pchisq(q=fi, df=p*(p+1)*(q-1)/2, lower.tail=FALSE)
+
+  method <- 'Box test for homogeneity of covariances'
+  statistic <- fi
+  names(statistic) <- c('phi')
+  parameter <- c(p*(p+1)*(q-1)/2)
+  names(parameter) <- c('df')
+  alternative <- "at least one covariance matrix is different \n"
+  estimate <- NULL
+  data.name <- 'this test uses summarized data'
+
+  return(list(statistic = statistic,
+              parameter = parameter,
+              p.value = p.value,
+              estimate = estimate,
+              alternative = alternative,
+              method = method,
+              data.name = data.name))
+}
--- R/plot.htest.R
+++ R/plot.htest.R
@@ -283,6 +283,27 @@ plot.htest <- function(x, col='red', shade.col='red', cex=0.8,
 
   }
 
+  ############# Test multiple covariance matrices ###############
+
+  if (x$method %in% c('Box test for homogeneity of covariances')) {
+
+    df <- x$parameter
+    st <- x$statistic
+    if (is.null(from)) from <- 0
+    if (is.null(to))     to <- 2 * st # Para sombrear hasta 2*stat
+
+    shade.dist(dist='dchisq', param=list(df=df),
+               b=st, type='upper', from=from, to=to, col.shadow=shade.col, ...)
+
+    leg <- bquote(paste(italic(X)," ~ ",chi^2,"(",.(df),")", sep = ""))
+    legend("top", bty="n", adj=0.5, legend=leg)
+
+    # To print the main title and the statistic
+    title(main='Shaded area corresponds to p-value')
+    mtext(text=round(st, digits=4), side=1, at=st, col=col, cex=cex, adj=0.5)
+
+  }
+
 
 }
 
