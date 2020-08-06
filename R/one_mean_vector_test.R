b41b2616e3532c09c537babde58acff9d99725c4
fhernanb <fhernanb@unal.edu.co>
2020-08-06 15:00:28 -0500

    se crearon los ejemplos externos para one_mean_vector_test

--- R/one_mean_vector_test.R
+++ R/one_mean_vector_test.R
@@ -21,57 +21,7 @@
 #' \item{method}{a character string indicating what type of test was performed.}
 #'
 #' @author Freddy Hernandez
-#' @examples
-#' # Example 5.2.2 from Rencher & Christensen (2012) page 127
-#' # Test H0: mu = (70, 170) versus H1: mu != (70, 170)
-#' # with known Sigma
-#'
-#' Sigma <- matrix(c(20, 100, 100, 1000), ncol=2, nrow=2)
-#'
-#' res1 <- one_mean_vector_test(mu0=c(70, 170), xbar=c(71.45, 164.7),
-#'                              n=20, Sigma=Sigma)
-#' res1
-#' plot(res1, from=4, to=10, shade.col='dodgerblue2')
-#'
-#' # Repeating the last example with raw data
-#' x1 <- c(69, 74, 68, 70, 72, 67, 66, 70, 76, 68,
-#'         72, 79, 74, 67, 66, 71, 74, 75, 75, 76)
-#' x2 <- c(153, 175, 155, 135, 172, 150, 115, 137, 200, 130,
-#'         140, 265, 185, 112, 140, 150, 165, 185, 210, 220)
-#' dt <- data.frame(x1, x2)
-#' mu0 <- c(70, 170)
-#' Sigma <- matrix(c(20, 100, 100, 1000), ncol=2, nrow=2)
-#'
-#' res2 <- one_mean_vector_test(mu0=mu0, xbar=colMeans(dt),
-#'                              n=nrow(dt), Sigma=Sigma)
-#' res2
-#' plot(res2, from=4, to=10, shade.col='lightpink1')
-#'
-#' # Example 5.2 from Johnson and Wichern (2012) page 214
-#' # Test H0: mu = (4, 50, 10) versus H1: mu != (4, 50, 10)
-#' # with unknown Sigma
-#'
-#' S <- matrix(c(2.879, 10.010, -1.810,
-#'               10.010, 199.788, -5.640,
-#'               -1.810, -5.640, 3.628), ncol=3, nrow=3)
-#'
-#' res3 <- one_mean_vector_test(mu0=c(4, 50, 10),
-#'                              xbar=c(4.640, 45.400, 9.965),
-#'                              n=20, S=S)
-#' res3
-#' plot(res3, from=0, to=5, shade.col='aquamarine3')
-#'
-#' \dontrun{
-#' library(rrcov)
-#' data(delivery)
-#' delivery.x <- delivery[, 1:2]
-#'
-#' # Using T2.test from rrcov package
-#' T2.test(delivery.x)
-#'
-#' one_mean_vector_test(mu0=c(0, 0), xbar=colMeans(delivery.x),
-#'                      n=25, S=var(delivery.x))
-#' }
+#' @example examples/examples_one_mean_vector_test.R
 #'
 #' @importFrom stats pf pchisq
 #' @export
--- examples/examples_two_mean_vector_test.R
+++ examples/examples_two_mean_vector_test.R
@@ -6,94 +6,92 @@ s1 <- matrix(c(5.192, 4.545, 6.522, 5.25,
                4.545, 13.18, 6.76, 6.266,
                6.522, 6.76, 28.67, 14.47,
                5.25, 6.266, 14.47, 16.65), ncol = 4)
-#'
+
 n2 <- 32
 xbar2 <- c(12.34, 13.91, 16.66, 21.94)
 s2 <- matrix(c(9.136, 7.549, 4.864, 4.151,
                7.549, 18.6, 10.22, 5.446,
                4.864, 10.22, 30.04, 13.49,
                4.151, 5.446, 13.49, 28), ncol = 4)
-#'
+
 res1 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = "T2")
 res1
 plot(res1, from=21, to=25, shade.col='tomato')
-#'
-#'
+
 # Example 3.7 from Seber (1984) page 116.
 # using the James first order test (1954).
 n1 <- 16
 xbar1 <- c(9.82, 15.06)
 s1 <- matrix(c(120, -16.3,
                -16.3, 17.8), ncol = 2)
-#'
+
 n2 <- 11
 xbar2 <- c(13.05, 22.57)
 s2 <- matrix(c(81.8, 32.1,
                32.1, 53.8), ncol = 2)
-#'
+
 res2 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'james', alpha=0.05)
 res2
 plot(res2, from=5, to=10, shade.col="lightgreen")
-#'
-#'
+
 # Example from page 141 from Yao (1965),
 # using Yao's test
-#'
+
 n1 <- 16
 xbar1 <- c(9.82, 15.06)
 s1 <- matrix(c(120, -16.3,
                -16.3, 17.8), ncol = 2)
-#'
+
 n2 <- 11
 xbar2 <- c(13.05, 22.57)
 s2 <- matrix(c(81.8, 32.1,
                32.1, 53.8), ncol = 2)
-#'
+
 res3 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'yao')
 res3
 plot(res3, from=2, to=6, shade.col="pink")
-#'
-#'
+
+
 # Example for Johansen's test using the data from
 # Example from page 141 from Yao (1965)
-#'
+
 res4 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'johansen')
 res4
 plot(res4, from=2, to=6, shade.col="aquamarine1")
-#'
+
 # Example 4.1 from Nel and Van de Merwe (1986) page 3729
 # Test H0: mu1 = mu2 versus H1: mu1 != mu2
 n1 <- 45
 xbar1 <- c(204.4, 556.6)
 s1 <- matrix(c(13825.3, 23823.4,
                23823.4, 73107.4), ncol=2)
-#'
+
 n2 <- 55
 xbar2 <- c(130.0, 355.0)
 s2 <- matrix(c(8632.0, 19616.7,
                19616.7, 55964.5), ncol=2)
-#'
+
 res5 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'nvm')
 res5
 plot(res5, from=6, to=10, shade.col='cyan2')
-#'
+
 # using mnvm method for same data
 res6 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'mnvm')
 res6
 plot(res6, from=6, to=10, shade.col='lightgoldenrodyellow')
-#'
+
 # using gamage method for same data
 res7 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
@@ -102,39 +100,34 @@ res7
 plot(res7, from=0, to=20, shade.col='mediumpurple4')
 text(x=10, y=0.30, "The curve corresponds to an empirical density",
      col='orange')
-#'
+
 # using Yanagihara and Yuan method for same data
-#'
 res8 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'yy')
 res8
 plot(res8, from=6, to=10, shade.col='gold2')
-#'
+
 # using Bartlett Correction method for same data
-#'
 res9 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                              xbar2 = xbar2, s2 = s2, n2 = n2,
                              method = 'byy')
 res9
-#'
+
 # using modified Bartlett Correction method for same data
-#'
 res10 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                               xbar2 = xbar2, s2 = s2, n2 = n2,
                               method = 'mbyy')
 res10
-#'
+
 # using Second Order Procedure (Kawasaki and Seo (2015)) method for same data
-#'
 res11 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                               xbar2 = xbar2, s2 = s2, n2 = n2,
                               method = 'ks1')
 res11
 plot(res11, from=6, to=10, shade.col='hotpink1')
-#'
+
 # using Bias Correction Procedure (Kawasaki and Seo (2015)) method for same data
-#'
 res12 <- two_mean_vector_test(xbar1 = xbar1, s1 = s1, n1 = n1,
                               xbar2 = xbar2, s2 = s2, n2 = n2,
                               method = 'ks2')
